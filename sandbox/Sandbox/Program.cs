450dfe4647b170aa13c11cde56da41d92adfe8b7
yourusername <youremail@>
2024-03-02 21:51:13 +0545

    finished prep1

--- sandbox/Sandbox/Program.cs
+++ sandbox/Sandbox/Program.cs
@@ -4,6 +4,11 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Sandbox World!");
+        Console.Write("What is your first name?  ");
+        string first=Console.ReadLine();
+        Console.Write("What is your last name?  ");
+        string last=Console.ReadLine();
+        Console.WriteLine($"Your name is {last},  {first} {last}");
+
     }
 }
