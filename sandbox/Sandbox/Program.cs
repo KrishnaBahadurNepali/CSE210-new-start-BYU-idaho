d33da1e2b08af46bf12830f634c0921ae33e0513
yourusername <117563556+KrishnaBahadurNepali@users.noreply.github.com>
2024-05-10 12:53:21 +0545

    completed sandbox

--- sandbox/Sandbox/Program.cs
+++ sandbox/Sandbox/Program.cs
@@ -9,6 +9,7 @@ class Program
         Console.Write("What is your last name?  ");
         string last=Console.ReadLine();
         Console.WriteLine($"Your name is {last},  {first} {last}");
+        Console.WriteLine("");
 
     }
 }
